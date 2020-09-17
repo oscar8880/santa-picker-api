b76e99e124355578f419c4cdfa4d8b8153c40ace
Oscar Zealley <oscar@zealley.com>
2020-09-17 21:21:47 +0100

    [OZ] update dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import * as express from 'express';
-import * as helmet from 'helmet';
-import * as cookieParser from 'cookie-parser';
-import * as logger from 'morgan';
-import * as cors from 'cors';
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+import * as express from "express";
+import * as helmet from "helmet";
+import * as cookieParser from "cookie-parser";
+import * as logger from "morgan";
+import * as cors from "cors";
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 
-import index from './routes/index';
-import email from './routes/email';
+import index from "./routes/index";
+import email from "./routes/email";
 
 const origin = process.env.WEB_ORIGIN;
 const app = express();
@@ -19,12 +19,12 @@ const checkJwt = jwt({
     jwksUri: `https://dev-k04yz4rs.eu.auth0.com/.well-known/jwks.json`,
   }),
 
-  audience: 'https://santa-picker',
+  audience: "https://santa-picker",
   issuer: `https://dev-k04yz4rs.eu.auth0.com/`,
-  algorithms: ['RS256'],
+  algorithms: ["RS256"],
 });
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -32,8 +32,8 @@ app.use(cors({ origin }));
 app.use(helmet());
 app.use(helmet.hidePoweredBy());
 
-app.use('/', index);
+app.use("/", index);
 app.use(checkJwt);
-app.use('/email', email);
+app.use("/email", email);
 
 export default app;
