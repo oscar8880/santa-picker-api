508cc286ace35261a9ea20c3bb90d0ead092f8ea
Oscar Zealley <oscar@zealley.com>
2020-09-18 14:29:42 +0100

    [OZ] update logging

--- src/routes/email.ts
+++ src/routes/email.ts
@@ -1,20 +1,22 @@
-import * as express from 'express';
-import { assignRandomContacts } from '../utils/random';
-const sgMail = require('@sendgrid/mail');
+import * as express from "express";
+import { assignRandomContacts } from "../utils/random";
+const sgMail = require("@sendgrid/mail");
 const router = express.Router();
-require('dotenv').config();
+require("dotenv").config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { contacts, organiserName, spendingLimit } = req.body;
   const pairedContacts = assignRandomContacts(contacts);
   const emails = pairedContacts.map((contactPair) => {
+    console.log(`Email request submitted by ${req.body.organiserName}`);
+    console.log(`Sending ${req.body.contacts.length} emails`);
     return {
       to: contactPair.contact.email,
-      from: 'secretsantacourierelf@gmail.com',
-      subject: 'Your Secret Santa Nomination',
-      templateId: 'd-b1e6786eee434135a63f3a106eabf4ad',
+      from: "secretsantacourierelf@gmail.com",
+      subject: "Your Secret Santa Nomination",
+      templateId: "d-b1e6786eee434135a63f3a106eabf4ad",
       dynamicTemplateData: {
         contactName: contactPair.contact.name,
         organiserName: organiserName,
@@ -22,18 +24,21 @@ router.post('/', async (req, res) => {
         spendingLimit:
           spendingLimit !== undefined
             ? `There's a recommended spend limit of ${spendingLimit}.`
-            : '',
+            : "",
       },
     };
   });
   sgMail
     .send(emails)
     .then((response) => {
-      console.log(response);
+      console.log(
+        `Successfully sent ${response.length}/${req.body.contacts.length} emails`
+      );
       const status = 200;
       res.status(status).json({ status });
     })
     .catch((error) => {
+      console.log(`Failed to send one or more emails`);
       console.log(error);
       const status = 500;
       res.status(status).json(error);
